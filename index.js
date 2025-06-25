642aff2128d123573b7710f95977cd2266d4314e
IanTuitoekk <iantuitoek4@gmail.com>
2025-06-25 12:48:59 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(arr, string) {
+  return arr.filter(
+    possibleMatch => possibleMatch.toLowerCase() === string.toLowerCase()
+  );
+}
+
+function fuzzyMatch(arr, string) {
+  return arr.filter(
+    possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+  );
+}
+
+function matchName(arr, stringName) {
+  return arr.filter(record => record.name === stringName);
+}
